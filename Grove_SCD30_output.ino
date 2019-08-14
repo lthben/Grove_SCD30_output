fbc8e1c065adcd1fc6ea3e9f3f8ba20076dc199a
Benjamin Low <lthben@gmail.com>
2019-08-14 14:41:31 +0800

    switched library
    switched to Sparkfun SCD30 library

--- Grove_SCD30_output.ino
+++ Grove_SCD30_output.ino
@@ -1,25 +1,23 @@
 //Connect sensor to I2C port of Grove shield
 //See PDF design guidelines on how to install the sensor
 //auto-calibration for first 7 days
+//RGB ring to D6
+//LCD to I2C
 
-#include "SCD30.h"
+//USER SETTINGS
+bool IS_CALIBRATED = true;//set to true once calibrated. NEED TO DO ONCE ONLY IN AIR CON ROOM
+int forcedCalibVal = 435;//forced calibration value in ppm
+int heightasl = 10;//height above sea level in metres
+int ambientAirPres = 1010;//air pressure in mBar
+int lowerCO2bound = 380; //to constrain the CO2 reading for meaningful pixel mapping
+int upperCO2bound = 450; //to constrain the CO2 reading for meaningful pixel mapping
+uint8_t wait = 100; //delay for the LED ring light sequence
 
-#if defined(ARDUINO_ARCH_AVR)
-#pragma message("Defined architecture for ARDUINO_ARCH_AVR.")
-#define SERIAL Serial
-#elif defined(ARDUINO_ARCH_SAM)
-#pragma message("Defined architecture for ARDUINO_ARCH_SAM.")
-#define SERIAL SerialUSB
-#elif defined(ARDUINO_ARCH_SAMD)
-#pragma message("Defined architecture for ARDUINO_ARCH_SAMD.")
-#define SERIAL SerialUSB
-#elif defined(ARDUINO_ARCH_STM32F4)
-#pragma message("Defined architecture for ARDUINO_ARCH_STM32F4.")
-#define SERIAL SerialUSB
-#else
-#pragma message("Not found any architecture.")
-#define SERIAL Serial
-#endif
+#include "SparkFun_SCD30_Arduino_Library.h"
+#include <Wire.h>
+
+SCD30 airSensor;
+long timeBegin;//time when the program starts
 
 #include "rgb_lcd.h"
 rgb_lcd lcd;
@@ -59,65 +57,93 @@ void colorWipe(uint8_t data, uint8_t wait)
 void setup()
 {
   Wire.begin();
-  SERIAL.begin(115200);
-  SERIAL.println("SCD30 Raw Data");
-  scd30.initialize();
-  //Calibration for minimum 7 days,after this ,close auto self calibration operation.
-  scd30.setAutoSelfCalibration(1);
+  Serial.begin(9600);
+
+  airSensor.begin(); //This will cause readings to occur every two seconds
+
+  //Change number of seconds between measurements: 2 to 1800 (30 minutes)
+  //While calibrating for 2 min, should set interval to 2 sec
+  airSensor.setMeasurementInterval(2);//interval of 5 screws up the reading
+
+  //My desk is ~1600m above sealevel
+  airSensor.setAltitudeCompensation(heightasl); //Set altitude of the sensor in m
+
+  //Pressure in Boulder, CO is 24.65inHg or 834.74mBar
+  airSensor.setAmbientPressure(ambientAirPres); //Current ambient pressure in mBar: 700 to 1200
 
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   lcd.setRGB(255, 255, 255); //set RGB colour and brightness here
   lcd.setCursor(0, 0);
-
   lcd.clear();
-  lcd.print("CO2 Sensor ready");
-  delay(2000);
+  if (!IS_CALIBRATED)
+  {
+    lcd.print("Initialising CO2 Sensor");
+    Serial.println("Initialising CO2 Sensor");
+  }
+  else
+  {
+    lcd.print("Sensor ready");
+    Serial.println("Sensor ready");
+    delay(2000);
+  }
 
   strip.begin();
   strip.setBrightness(255);
   strip.show(); // Initialize all pixels to 'off'
+
+  timeBegin = millis();
 }
 
 void loop()
 {
-  float result[3] = {0};
-
-  if (scd30.isAvailable())
-  {
-    scd30.getCarbonDioxideConcentration(result);
-    SERIAL.print("Carbon Dioxide Concentration is: ");
-    SERIAL.print(result[0]);
-    SERIAL.println(" ppm");
-    SERIAL.println(" ");
-    //    SERIAL.print("Temperature = ");
-    //    SERIAL.print(result[1]);
-    //    SERIAL.println(" ℃");
-    //    SERIAL.println(" ");
-    //    SERIAL.print("Humidity = ");
-    //    SERIAL.print(result[2]);
-    //    SERIAL.println(" %");
-    //    SERIAL.println(" ");
-    //    SERIAL.println(" ");
-    //    SERIAL.println(" ");
+  if (!IS_CALIBRATED) {
+    if (millis() - timeBegin > 150000) //2.5 min
+    {
+      airSensor.setForcedRecalibrationFactor(forcedCalibVal);//global ppm is 415ppm
+      IS_CALIBRATED = true;
+    }
   }
 
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("CO2 reading:");
-  lcd.setCursor(0, 1);
-  lcd.print(result[0]);
-  lcd.print(" ppm");
+  int CO2reading;
+  float humidReading, tempReading;
 
-  int data = constrain(result[0], 380, 450);//constrain ppm
-  data = map(data, 380, 450, 0, 19);//convert 380-450ppm to 0-19 leds
-  uint8_t wait = 100;
+  if (airSensor.dataAvailable())
+  {
+    CO2reading =  airSensor.getCO2();
+    tempReading = airSensor.getTemperature();
+    humidReading = airSensor.getHumidity();
 
-  strip.clear();
-  strip.show();//turn off
-//  delay(500);
+    if (IS_CALIBRATED)
+    {
+      Serial.print("co2(ppm):");
+      Serial.print(CO2reading);
 
-  colorWipe(data, wait);
+      Serial.print(" temp(C):");
+      Serial.print(tempReading, 1);
 
-  delay(5000);
+      Serial.print(" humidity(%):");
+      Serial.print(humidReading, 1);
+
+      Serial.println();
+
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("CO2 reading:");
+      lcd.setCursor(0, 1);
+      lcd.print(CO2reading);
+      lcd.print(" ppm");
+
+      int data = constrain(CO2reading, lowerCO2bound, upperCO2bound);//constrain ppm
+      data = map(data, lowerCO2bound, upperCO2bound, 0, 19);//convert 380-450ppm to 0-19 leds
+
+      strip.clear();
+      strip.show();//turn off
+      //  delay(500);
+
+      colorWipe(data, wait);
+    }
+    else Serial.println("Initialising sensor ... ");
+  }
+  delay(2000);//note that the sensor already has delay
 }
